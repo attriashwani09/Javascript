c13989b4076a3a72852e7ec841f196eddff9cf87
attriashwani09 <attriashwani09@gmail.com>
2025-11-14 21:51:52 +0530

    Scope in js

---
+++ lc 11 Loops And Variable Scope/Loops.js
@@ -0,0 +1,18 @@
+// Loops 
+
+// 1). For Loop :
+let num=10;
+
+for(let i=0;i<num;i++){
+    console.log(i);
+} 
+
+// 2). Nested for Loop 
+
+for(let i=0;i<5;i++){
+    for(let j=0;j<5;j++){
+        console.log(j);
+    }
+} 
+
+
