ed028602dbb9689ac5af736738413943a16d51b3
attriashwani09 <attriashwani09@gmail.com>
2025-11-20 09:29:25 +0530

    Dom Day 1

---
+++ lc 16 DOM/first.js
@@ -0,0 +1,6 @@
+// document.getElementById('first').innerHTML="Honey Singh "
+
+const obj=document.getElementById("first");
+console.log(obj); 
+
+obj.className="Mohan"   // class name chnages to mohan
